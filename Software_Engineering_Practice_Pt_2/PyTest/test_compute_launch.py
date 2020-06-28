2ca1038bf738fe4b92579b8f147afdd7a6ad9d80
Vanditg <gajjarvandit@gmail.com>
2020-06-29 00:22:31 +0930

    Adding Software-Engineering-Practices-Pt-2 Quiz and PyTest Python files

---
+++ Software_Engineering_Practice_Pt_2/PyTest/test_compute_launch.py
@@ -0,0 +1,20 @@
+##==================================
+## AWS Machine Learning Foundation Course
+## Lesson - 2: Software Engineering Practices Pt II
+## Year: 2020
+## Sub-lesson: Unit Tests 
+##===================================
+
+from compute_launch import days_until_launch
+
+def test_days_until_launch_4():
+    assert(days_until_launch(22, 26) == 4)
+
+def test_days_until_launch_0():
+    assert(days_until_launch(253, 253) == 0)
+
+def test_days_until_launch_0_negative():
+    assert(days_until_launch(83, 64) == 0)
+    
+def test_days_until_launch_1():
+    assert(days_until_launch(9, 10) == 1)
